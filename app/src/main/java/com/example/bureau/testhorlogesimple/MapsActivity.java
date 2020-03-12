26464f0f005100613d6f17ecde4b79f9b4ca1aa4
Louis <louislenezet@gmail.com>
2020-03-12 16:51:46 +0100

    Geofencing API
    All proximity alert replaced by geofence functions
    Worked correctly better overview.
    Possibility to change updates parameters (less battery consumption).
    Need to fix background usage

--- app/src/main/java/com/example/bureau/testhorlogesimple/MapsActivity.java
+++ app/src/main/java/com/example/bureau/testhorlogesimple/MapsActivity.java
@@ -1,15 +1,21 @@
 package com.example.bureau.testhorlogesimple;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -20,7 +26,14 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -31,23 +44,28 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+
+import static com.google.android.gms.location.Geofence.NEVER_EXPIRE;
+
 
 public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback {
+        implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        OnMapReadyCallback,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener,
+        LocationListener {
+
     //Variable preferences
     public static final String MY_PREF="mesPrefs";
     SharedPreferences myVar;
     SharedPreferences.Editor myVarEditor;
-
-    //Variable activity
-    private static final String TAG = MapsActivity.class.getSimpleName();
-    private GoogleMap mMap;
-    LocationManager locationManager;
-    PendingIntent pendingIntentLocation;
-
     int zoneNumGPS = 4;
-    Marker[] markersArray = new Marker[4];
-    Circle[] circlesArray = new Circle[4];
     final String[] positionID = {"Famille", "Travail", "Joker", "A la maison"};
     String lat="";
     String lng = "";
@@ -55,15 +73,54 @@ public class MapsActivity extends FragmentActivity
     String title = "";
     Boolean gpsDefine = false;
 
+    //Variable activity
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    Context appContext;
+    //Map variable
+    private TextView textLat, textLong;
+    private GoogleMap mMap;
+    Marker[] markersArray = new Marker[4];
+    Circle[] circlesArray = new Circle[4];
+
+    //Location
+    LocationManager locationManager;
+    private Location lastLocation;
+    private LocationRequest locationRequest;
+    private final int UPDATE_INTERVAL =  1000;// Defined in mili seconds.
+    private final int FASTEST_INTERVAL = 900;// This number in extremely low, and should be used only for debug
+
+    //Geofencing API
+    private GoogleApiClient googleApiClient;
+    private GeofencingClient geofencingClient;
+    ArrayList<Geofence> geofenceList= new ArrayList<Geofence>(4);
+    PendingIntent geofencePendingIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        appContext=getApplicationContext();
         setContentView(R.layout.activity_maps);
         myVar = getSharedPreferences(MY_PREF, Context.MODE_PRIVATE);
         myVarEditor = myVar.edit();
+        textLat = (TextView) findViewById(R.id.Lat);
+        textLong = (TextView) findViewById(R.id.Long);
         SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        createGoogleApi();
+        geofencingClient = LocationServices.getGeofencingClient(appContext);
+    }
+
+    // Create GoogleApiClient instance
+    private void createGoogleApi() {
+        Log.d(TAG, "createGoogleApi()");
+        if ( googleApiClient == null ) {
+            googleApiClient = new GoogleApiClient.Builder( appContext )
+                    .addConnectionCallbacks( this )
+                    .addOnConnectionFailedListener( this )
+                    .addApi( LocationServices.API )
+                    .build();
+        }
     }
 
     @Override
@@ -132,12 +189,10 @@ public class MapsActivity extends FragmentActivity
                                 txtView.setText(radiusValue + " mètres");
                                 radiusSize[0] = radiusValue;
                             }
-
                             public void onStartTrackingTouch(SeekBar arg0) {
                                 // TODO Auto-generated method stub
 
                             }
-
                             public void onStopTrackingTouch(SeekBar seekBar) {
                                 // TODO Auto-generated method stub
 
@@ -154,7 +209,7 @@ public class MapsActivity extends FragmentActivity
                                         // Drawing circle on the map
                                         modifyCircle(pointing, circlesArray[zoneNumGPS], radiusSize[0]);
                                         // Add proximity
-                                        setProximityAlert(pointing,radiusSize[0],zoneNumGPS);
+                                        setGeofence(pointing,radiusSize[0],zoneNumGPS);
                                         // Storing the latitude for the i-th location
                                         myVarEditor.putString("lat" + positionID[zoneNumGPS], Double.toString(pointing.latitude));
                                         // Storing the longitude for the i-th location
@@ -210,7 +265,7 @@ public class MapsActivity extends FragmentActivity
                         markersArray[zoneNumGPS].setVisible(false);
                         circlesArray[zoneNumGPS].setVisible(false);
                         // Remove the proximity alert
-                        removeProximityAlert(zoneNumGPS);
+                        removeGeofence(zoneNumGPS);
                         // Confirming GPS non definition to the shared preferences
                         myVarEditor.putBoolean("gpsDefine" + positionID[zoneNumGPS], false);
                         // Delete position on zone from sharedPreference
@@ -233,6 +288,7 @@ public class MapsActivity extends FragmentActivity
         });
     }
 
+    //Circles and Marker edition functions
     private void drawMarker(LatLng point, String title, int i, Boolean gpsDefine) {
         TypedArray ta = getResources().obtainTypedArray(R.array.colors);
         int colorToUse = ta.getResourceId(i, 0);
@@ -267,50 +323,215 @@ public class MapsActivity extends FragmentActivity
         circle.setVisible(true);
     }
 
+    //Geofences edition function
+    private void setGeofence(LatLng point, int radius,int id){
+        geofenceList.add(0,new Geofence.Builder()
+                .setRequestId(String.valueOf(id))
+                .setCircularRegion(point.latitude,point.longitude,radius)
+                .setExpirationDuration(NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER|Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build());
+
+        geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent(id))
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Sucess", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+    private void removeGeofence(int id){
+        geofencingClient.removeGeofences(getGeofencePendingIntent(id))
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getBaseContext(), "Sucess to delete", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getBaseContext(), "Failed to delete", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER|Geofence.GEOFENCE_TRANSITION_EXIT);
+        builder.addGeofences(geofenceList);
+        return builder.build();
+    }
+    private PendingIntent getGeofencePendingIntent(int id) {
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(appContext, ProximityReceiver.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        geofencePendingIntent = PendingIntent.getBroadcast(appContext, id, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
+    }
+
+
+    // GoogleApiClient.ConnectionCallbacks connected
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "onConnected()");
+        getLastKnownLocation();
+    }
+    // GoogleApiClient.ConnectionCallbacks suspended
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, "onConnectionSuspended()");
+    }
+    // GoogleApiClient.OnConnectionFailedListener fail
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.w(TAG, "onConnectionFailed()");
+    }
+
+    //Change in location functions
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged ["+location+"]");
+        lastLocation = location;
+        writeActualLocation(location);
+    }
+    // Get last known location
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation()");
+        if ( checkPermission() ) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if ( lastLocation != null ) {
+                Log.i(TAG, "LasKnown location. " +
+                        "Long: " + lastLocation.getLongitude() +
+                        " | Lat: " + lastLocation.getLatitude());
+                writeLastLocation();
+                startLocationUpdates();
+            } else {
+                Log.w(TAG, "No location retrieved yet");
+                startLocationUpdates();
+            }
+        }
+        else askPermission();
+    }
+    // Start location Updates
+    private void startLocationUpdates(){
+        Log.i(TAG, "startLocationUpdates()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_LOW_POWER)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+        if ( checkPermission() ) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
+    }
+    // Write location coordinates on UI
+    private void writeActualLocation(Location location) {
+        textLat.setText( "Lat: " + location.getLatitude() );
+        textLong.setText( "Long: " + location.getLongitude() );
+    }
+    private void writeLastLocation() {
+        writeActualLocation(lastLocation);
+    }
+
+    //Permissions
+    // Verify user's response of the permission requested
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch ( requestCode ) {
+            case 1: {
+                if ( grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                    // Permission granted
+                    getLastKnownLocation();
+
+                } else {
+                    // Permission denied
+                    permissionsDenied();
+                }
+                break;
+            }
+        }
+    }
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+    // Asks for permission
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] { Manifest.permission.ACCESS_FINE_LOCATION },1
+        );
+    }
+    // App cannot work without the permissions
+    private void permissionsDenied() {
+        Log.w(TAG, "permissionsDenied()");
+    }
+
+    // Callback called when Map is touched
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.d(TAG, "onMapClick("+latLng +")");
+    }
+    // Callback called when Marker is touched
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.d(TAG, "onMarkerClickListener: " + marker.getPosition() );
+        return false;
+    }
+
+    //Get back functions
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(MapsActivity.this, Main.class));
+    }
     public void getBack(View view) {
         startActivity(new Intent(MapsActivity.this, Main.class));
     }
 
-    public void setProximityAlert(LatLng pointing,int radiusSize,int id){
-        // This intent will call the activity ProximityActivity
-        Intent proximityIntent = new Intent(getApplicationContext(),ProximityReceiver.class);
-        proximityIntent.putExtra("name",positionID[id]);
-        proximityIntent.putExtra("id",id);
-        // Creating a pending intent which will be invoked by LocationManager when the specified region is
-        // entered or exited
-        pendingIntentLocation = PendingIntent.getBroadcast(getApplicationContext(), id, proximityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        // Setting proximity alert
-        // The pending intent will be invoked when the device enters or exits the region 20 meters
-        // away from the marked point
-        // The -1 indicates that, the monitor will not be expired
-        try {
-            locationManager.addProximityAlert(pointing.latitude, pointing.longitude, radiusSize, -1, pendingIntentLocation);
-        } catch (SecurityException e) {
-        }
-    }
-    public void removeProximityAlert(int id){
-        Intent proximityIntent = new Intent(getApplicationContext(),ProximityReceiver.class);
-        proximityIntent.putExtra("name",positionID[id]);
-        proximityIntent.putExtra("id",id);
-        pendingIntentLocation = PendingIntent.getBroadcast(getApplicationContext(), id, proximityIntent,PendingIntent.FLAG_CANCEL_CURRENT);
-        // Removing the proximity alert
-        locationManager.removeProximityAlert(pendingIntentLocation);
-    }
 
     @Override
-    public void onBackPressed() {
-        startActivity(new Intent(MapsActivity.this, Main.class));
+    protected void onStart() {
+        super.onStart();
+        if(!checkPermission()){
+            askPermission();
+        }else {
+            // Call GoogleApiClient connection when starting the Activity
+            googleApiClient.connect();
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        /*
+        // Disconnect GoogleApiClient when stopping Activity
+        googleApiClient.disconnect();
+         */
+
     }
     @Override
     public void onResume() {
         super.onResume();
     }
     @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-   public void onPause() {
+    public void onPause() {
         super.onPause();
     }
 }
--- app/src/main/java/com/example/bureau/testhorlogesimple/ProximityReceiver.java
+++ app/src/main/java/com/example/bureau/testhorlogesimple/ProximityReceiver.java
@@ -1,6 +1,5 @@
 package com.example.bureau.testhorlogesimple;
 
-import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,7 +7,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
@@ -16,10 +14,18 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 
 public class ProximityReceiver extends BroadcastReceiver {
     public static final String MY_PREF="mesPrefs";
+    final String[] positionID = {"Famille", "Travail", "Joker", "A la maison"};
     SharedPreferences myVar;
     SharedPreferences.Editor myVarEditor;
     String notificationTitle;
@@ -35,61 +41,78 @@ public class ProximityReceiver extends BroadcastReceiver {
          */
         // TODO Auto-generated method stub
         Log.d("Intent1","Intent received");
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage = GeofenceStatusCodes.getStatusCodeString(geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMessage);
+            return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
         myVar=context.getSharedPreferences(MY_PREF,context.MODE_PRIVATE);
         myVarEditor = myVar.edit();
         Boolean notifDisplay=myVar.getBoolean("notifsEnable",true);
 
-        Boolean proximity_entering = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false);
-        String proximity_name = intent.getStringExtra("name");
 
-        if(proximity_entering){
-            Toast.makeText(context,"Entering the region"  ,Toast.LENGTH_LONG).show();
-            notificationTitle="Proximity - Entry";
-            notificationContent= proximity_name;
-            tickerMessage = proximity_name;
-            myVarEditor.putBoolean("in"+proximity_name+"GPS",true);
-            myVarEditor.commit();
-        }else{
-            Toast.makeText(context,"Exiting the region"  ,Toast.LENGTH_LONG).show();
-            notificationTitle="Proximity - Exit";
-            notificationContent=proximity_name;
-            tickerMessage = proximity_name;
-            myVarEditor.putBoolean("in"+proximity_name+"GPS",false);
-            myVarEditor.commit();
-        }
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
 
-        if(notifDisplay) {
-            Intent notificationIntent = null;
-            notificationIntent = new Intent(context, com.example.bureau.testhorlogesimple.NotificationView.class);
-            notificationIntent.putExtra("content", notificationContent);
+            // Get the geofences that were triggered.
+            // !! A single event can trigger multiple geofences.!!
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+            String proximity_name = positionID[Integer.parseInt(triggeringGeofences.get(0).getRequestId())];
 
-            /** This is needed to make this intent different from its previous intents */
-            notificationIntent.setData(Uri.parse("tel:/" + (int) System.currentTimeMillis()));
+            if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                Toast.makeText(context, "Entering the region", Toast.LENGTH_LONG).show();
+                notificationTitle = "Proximity - Entry";
+                notificationContent = proximity_name;
+                tickerMessage = proximity_name;
+                myVarEditor.putBoolean("in" + proximity_name + "GPS", true);
+                myVarEditor.commit();
+            } else {
+                Toast.makeText(context, "Exiting the region", Toast.LENGTH_LONG).show();
+                notificationTitle = "Proximity - Exit";
+                notificationContent = proximity_name;
+                tickerMessage = proximity_name;
+                myVarEditor.putBoolean("in" + proximity_name + "GPS", false);
+                myVarEditor.commit();
+            }
 
-            /** Creating different tasks for each notification. See the flag Intent.FLAG_ACTIVITY_NEW_TASK */
-            PendingIntent pendingIntentNotif = PendingIntent.getActivity(context.getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            if (notifDisplay) {
+                Intent notificationIntent = null;
+                notificationIntent = new Intent(context, com.example.bureau.testhorlogesimple.NotificationView.class);
+                notificationIntent.putExtra("content", notificationContent);
 
-            /** Getting the System service NotificationManager */
-            NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                /** This is needed to make this intent different from its previous intents */
+                notificationIntent.setData(Uri.parse("tel:/" + (int) System.currentTimeMillis()));
 
-            /** Configuring notification builder to create a notification */
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
-                    .setWhen(System.currentTimeMillis())
-                    .setContentText(notificationContent)
-                    .setContentTitle(notificationTitle)
-                    .setSmallIcon(R.mipmap.chenillelogo)
-                    .setAutoCancel(true)
-                    .setTicker(tickerMessage)
-                    .setContentIntent(pendingIntentNotif);
+                /** Creating different tasks for each notification. See the flag Intent.FLAG_ACTIVITY_NEW_TASK */
+                PendingIntent pendingIntentNotif = PendingIntent.getActivity(context.getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-            /** Creating a notification from the notification builder */
-            Notification notification = notificationBuilder.build();
+                /** Getting the System service NotificationManager */
+                NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-            /** Sending the notification to system.
-             * The first argument ensures that each notification is having a unique id
-             * If two notifications share same notification id, then the last notification replaces the first notification
-             * */
-            nManager.notify((int) System.currentTimeMillis(), notification);
+                /** Configuring notification builder to create a notification */
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+                        .setWhen(System.currentTimeMillis())
+                        .setContentText(notificationContent)
+                        .setContentTitle(notificationTitle)
+                        .setSmallIcon(R.mipmap.chenillelogo)
+                        .setAutoCancel(true)
+                        .setTicker(tickerMessage)
+                        .setContentIntent(pendingIntentNotif);
+
+                /** Creating a notification from the notification builder */
+                Notification notification = notificationBuilder.build();
+
+                /** Sending the notification to system.
+                 * The first argument ensures that each notification is having a unique id
+                 * If two notifications share same notification id, then the last notification replaces the first notification
+                 * */
+                nManager.notify((int) System.currentTimeMillis(), notification);
+            }
         }
     }
 }
